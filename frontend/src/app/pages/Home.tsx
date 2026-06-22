5cb4acbe08d049d51768ed5ccafd68b313474433
MiguelAR07 <miguelarcila008@gmail.com>
2026-06-21 22:16:41 -0500

    Fix typescript build error

--- frontend/src/app/pages/Home.tsx
+++ frontend/src/app/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { CabinsSection } from "../components/CabinsSection";
 import PromotionsSection from "../components/PromotionsSection";
 import { Link, useLocation } from "react-router-dom";
